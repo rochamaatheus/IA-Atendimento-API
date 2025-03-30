bfa64f90aed3226843e7b439e2689148c3c65a6d
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2025-03-30 14:13:01 -0300

    middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import { parseBodyMiddleware } from './lib/middlewares.js';
 import calendarRoutes from './api/calendar.js';
 import sheetsRoutes from './api/sheets.js';
 import confirmarRoutes from './api/confirmar.js';
@@ -10,6 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(parseBodyMiddleware);
 app.use('/api', calendarRoutes);
 app.use('/api', sheetsRoutes);
 app.use('/api', confirmarRoutes);
